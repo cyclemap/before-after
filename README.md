a5e51774c31bc913b69a8f2a02b55b7b7fb21233
adrian <adrian-sw@aporter.org>
2023-12-09 15:56:11 -0500

    a "before and after" map.

---
+++ README.md
@@ -0,0 +1,12 @@
+
+* [view this content](https://cyclemaps.org/?geo=https://cyclemap.github.io/before-after/main.geojson&type=symbol)
+
+* Locations are approximate:  only the city location was specified.
+
+* This is not my content:  they are links you can follow to see the owner of the content.
+
+* "up" arrows highlight an improvement
+
+* "down" arrows highlight a regression
+
+
---
+++ update.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+<input.geojson \
+>main.geojson \
+jq \
+	--compact-output \
+	'
+def provideContent:
+	"<a target=\"_blank\" href=\"" + . + "\">" + (
+		if endswith(".jpg") or endswith(".png") then
+			"<img style=\"width: 58em\" src=\"" + . + "\" />"
+		else
+			"<b>link</b>"
+		end
+	) + "</a>";
+
+	
+.features |= map(select(.properties.type != "neither")) |
+.features[].properties |= (
+	. += {
+		"title": .location,
+		"description": (
+			"<div style=\"positon: absolute; overflow-y:auto; overflow-x: hidden; width: 60em; height: 50em;\">" +
+
+			"<h3>" + .location + "</h3>" +
+			
+			(.data |map("<p>" + provideContent + "</p>") |join("") ) +
+			
+			"</div>"
+		)
+	}
+)
+'
+
+
